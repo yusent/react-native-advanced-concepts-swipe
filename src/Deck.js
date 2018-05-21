1e19e45c8f2c5b31fb181645365328002d0e4c7f
Yusent Chig <yusent@protonmail.com>
2018-05-21 09:40:42 -0700

    Handle gestures on first card

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { Animated, View } from 'react-native';
+import { Animated, PanResponder, View } from 'react-native';
 
 export default class Deck extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._position = new Animated.ValueXY();
+
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        this._position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {},
+    });
+  }
+
   _renderCards() {
-    return this.props.data.map(this.props.renderCard);
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={ item.id }
+            style={ this._position.getLayout() }
+            { ...this._panResponder.panHandlers }
+          >
+            { this.props.renderCard(item) }
+          </Animated.View>
+        );
+      }
+
+      return this.props.renderCard(item);
+    });
   }
 
   render() {
